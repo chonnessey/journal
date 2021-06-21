ae112bc4fe1c3cd699cb8949bf565e902e7e549c
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-21 11:23:59 -0700

    add two new functions to journal.js
    Co-authored-by: Dave Lindqvist <Lindqvist.Dave@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -1,16 +1,37 @@
 export default function Journal(title, body) {
   this.title = title;
   this.body = body;
+  this.counter = 0;
+  this.vowels = 0;
+  this.consonants = 0;
 }
 
 Journal.prototype.addAllWords = function () {
   let titleEntry = this.title.split(' ');
   let bodyEntry = this.body.split(' ');
   let totalWords = titleEntry.length + bodyEntry.length;
-  
-  // console.log(titleEntry);
-  // console.log(bodyEntry);
-  console.log(totalWords);
+  this.counter += totalWords;
+  console.log(this.counter);
 }
+
+Journal.prototype.tallyVowels = function () {
+  const totalTitleVowels = this.title.replace(/[^aeiou]/gi, "");
+  const totalBodyVowels = this.body.replace(/[^aeiou]/gi, "");
+  const totalVowels = totalTitleVowels.length + totalBodyVowels.length;
+  this.vowels += totalVowels;
+  console.log(this.vowels);
+}
+
+Journal.prototype.tallyConsonants = function () {
+  const totalTitleConsonants = this.title.replace(/[aeiou]/gi, "");
+  const totalBodyConsonants = this.body.replace(/[aeiou]/gi, "");
+  const totalConsonants = totalTitleConsonants.length + totalBodyConsonants.length;
+  this.consonants += totalConsonants;
+  console.log(this.consonants);
+}
+
 let newJournal = new Journal("hello there", "what's up")
 newJournal.addAllWords();
+newJournal.tallyVowels();
+newJournal.tallyConsonants();
+
