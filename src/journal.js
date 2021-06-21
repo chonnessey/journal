9169b06f3d6d694d2bf9234f708a17f3b01129f2
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-21 10:51:23 -0700

    finish addAllWords function
    Co-authored-by: Dave Lindqvist <Lindqvist.Dave@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -6,10 +6,11 @@ export default function Journal(title, body) {
 Journal.prototype.addAllWords = function () {
   let titleEntry = this.title.split(' ');
   let bodyEntry = this.body.split(' ');
+  let totalWords = titleEntry.length + bodyEntry.length;
   
-  
-  console.log(titleEntry);
-  console.log(bodyEntry);
+  // console.log(titleEntry);
+  // console.log(bodyEntry);
+  console.log(totalWords);
 }
 let newJournal = new Journal("hello there", "what's up")
 newJournal.addAllWords();
